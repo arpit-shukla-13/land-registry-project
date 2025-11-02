6e86da30157cc79e81aaaae2ca8d095fb8411e3e
arpit-shukla-13 <arpitshukla702@gmail.com>
2025-11-02 23:05:21 +0530

    update

--- server/index.js
+++ server/index.js
@@ -16,6 +16,7 @@ const MONGO_URI = process.env.MONGO_URI;
 mongoose.connect(MONGO_URI)
   .then(() => console.log("Successfully connected to MongoDB!"))
   .catch(err => console.error("MongoDB connection error:", err));
+  
 
 // --- API Routes ---
 
